68ed381001122e68203849e835134e8d5ae44e43
sandyrev igor <sandyrev.igor@gmail.com>
2022-07-24 01:25:27 +0500

    reinst commit

--- src/components/message/message.js
+++
@@ -1,9 +0,0 @@
-import * as Handlebars from "handlebars";
-import message from "./message.hbs"
-import "./message.scss"
-
-Handlebars.registerPartial("message", message)
-
-export default (props) => {
-    return message(props);
-}
