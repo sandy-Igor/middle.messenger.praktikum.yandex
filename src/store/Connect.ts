a8b3d4abd5e561a3774e60b288cedeaec4ec897f
sandyrev igor <sandyrev.igor@gmail.com>
2022-09-18 02:44:05 +0500

    fixes

--- src/store/Connect.ts
+++ src/store/Connect.ts
@@ -2,7 +2,7 @@ import Block, { Props } from '../block';
 import { Indexed } from "../utils/set";
 import store, { StoreEvents } from "./Store";
 
-export default function connect(Component: typeof Block<Props>, mapStateToProps: (state: Indexed) => Indexed) {
+export default function сonnect(Component: typeof Block<Props>, mapStateToProps: (state: Indexed) => Indexed) {
     return class extends Component {
         constructor(props: Props = {}) {
             super('div',{...props, ...mapStateToProps(store.getState())});
